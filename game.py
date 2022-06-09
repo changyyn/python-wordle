3f1b137fe66187a3d1db49408036c594c3f589db
Chang <47656782+changyyn@users.noreply.github.com>
2022-06-09 10:31:45 -0700

    Game is playable by launching main.py

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from color import Color
 from wordle import Wordle
 
 
@@ -6,7 +7,7 @@ Class for the actual game that will use the wordle class to play the game
 """
 class Game():
 
-    def __init__(self, word, max_guesses=5):
+    def __init__(self, word, max_guesses=6):
         self.wordle : Wordle = Wordle(word)
         self.max_guesses = max_guesses
         self.results = []
@@ -17,6 +18,10 @@ class Game():
         
         self.guesses.append(guess_word)
         result = self.wordle.get_result_from_guess(guess_word)
+
+        if Color.BLACK not in result and Color.YELLOW not in result:
+            self.is_win = True
+
         self.results.append(result)
         
     def get_results_str(self):
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from game import Game
+
 def main():
-    print("Hello Worldle")
+
+    game = Game('trout')
+
+    while not game.has_won() and not game.has_lost():
+
+        guess = input("Guess a five letter word: ")
+        
+        game.guess(guess)
+
+        print(game.get_results_str())
+
+    if game.has_won():
+        print("Conglaturation you have won!")
+    else:
+        print("Nice try :)")
+
+
 
 if __name__ == '__main__':
     main()
